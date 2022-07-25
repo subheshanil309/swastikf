5ee48e175bd113ab63e68d99c5c3ae1b9050b8aa
unknown <bitmegatasktechno@gmail.com>
2022-07-25 11:20:17 +0530

    added profile changes access and change your password

--- application/views/admin/includes/header.php
+++ application/views/admin/includes/header.php
@@ -38,7 +38,7 @@
          <div class="navbar-header">
             <div class="d-flex">
                <!-- LOGO -->
-               <div class="navbar-brand-box">
+               <div class="navbar-brand-box" style="padding: 0">
                   <a href="<?php echo base_url(); ?>admin" class="logo logo-dark">
                   <span class="logo-sm">
                   <img src="<?php echo base_url(); ?>assets/admin/images/logo.svg" alt="" height="22">
@@ -59,19 +59,14 @@
                <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect" id="vertical-menu-btn">
                <i class="fa fa-fw fa-bars"></i>
                </button>
-               <!-- App Search-->
-               <form class="app-search d-none d-lg-block">
-                  <div class="position-relative">
-                     <input type="text" class="form-control" placeholder="Search...">
-                     <span class="bx bx-search-alt"></span>
-                  </div>
-               </form>
-               <div class="dropdown dropdown-mega d-none d-lg-block ms-2">
-                  <button type="button" class="btn header-item waves-effect" data-bs-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
-                  <span key="t-megamenu">Mega Menu</span>
-                  <i class="mdi mdi-chevron-down"></i> 
-                  </button>
-               </div>
+               <div class="dropdown d-none d-lg-inline-block ms-1">
+                            <button type="button" onclick="javascript:window.location.href='<?php echo base_url()?>admin/dashboard'" class="btn header-item "  >
+                                <div class="text-success">SWASTIK AGRO BIOTECH & PLANTATION</div>
+                            </button>
+                        </div>
+              
+                
+                
             </div>
             <div class="d-flex">
                <div class="dropdown d-inline-block d-lg-none ms-2">
@@ -102,7 +97,7 @@
                   <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-notifications-dropdown"
                      data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <i class="bx bx-bell bx-tada"></i>
-                  <span class="badge bg-danger rounded-pill">3</span>
+                  <span class="badge bg-danger rounded-pill">0</span>
                   </button>
                   <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
                      aria-labelledby="page-header-notifications-dropdown">
@@ -117,64 +112,7 @@
                         </div>
                      </div>
                      <div data-simplebar style="max-height: 230px;">
-                        <a href="javascript: void(0);" class="text-reset notification-item">
-                           <div class="d-flex">
-                              <div class="avatar-xs me-3">
-                                 <span class="avatar-title bg-primary rounded-circle font-size-16">
-                                 <i class="bx bx-cart"></i>
-                                 </span>
-                              </div>
-                              <div class="flex-grow-1">
-                                 <h6 class="mb-1" key="t-your-order">Your order is placed</h6>
-                                 <div class="font-size-12 text-muted">
-                                    <p class="mb-1" key="t-grammer">If several languages coalesce the grammar</p>
-                                    <p class="mb-0"><i class="mdi mdi-clock-outline"></i> <span key="t-min-ago">3 min ago</span></p>
-                                 </div>
-                              </div>
-                           </div>
-                        </a>
-                        <a href="javascript: void(0);" class="text-reset notification-item">
-                           <div class="d-flex">
-                              <img src="<?php echo base_url(); ?>assets/admin/images/users/avatar-3.jpg"
-                                 class="me-3 rounded-circle avatar-xs" alt="user-pic">
-                              <div class="flex-grow-1">
-                                 <h6 class="mb-1">James Lemire</h6>
-                                 <div class="font-size-12 text-muted">
-                                    <p class="mb-1" key="t-simplified">It will seem like simplified English.</p>
-                                    <p class="mb-0"><i class="mdi mdi-clock-outline"></i> <span key="t-hours-ago">1 hours ago</span></p>
-                                 </div>
-                              </div>
-                           </div>
-                        </a>
-                        <a href="javascript: void(0);" class="text-reset notification-item">
-                           <div class="d-flex">
-                              <div class="avatar-xs me-3">
-                                 <span class="avatar-title bg-success rounded-circle font-size-16">
-                                 <i class="bx bx-badge-check"></i>
-                                 </span>
-                              </div>
-                              <div class="flex-grow-1">
-                                 <h6 class="mb-1" key="t-shipped">Your item is shipped</h6>
-                                 <div class="font-size-12 text-muted">
-                                    <p class="mb-1" key="t-grammer">If several languages coalesce the grammar</p>
-                                    <p class="mb-0"><i class="mdi mdi-clock-outline"></i> <span key="t-min-ago">3 min ago</span></p>
-                                 </div>
-                              </div>
-                           </div>
-                        </a>
-                        <a href="javascript: void(0);" class="text-reset notification-item">
-                           <div class="d-flex">
-                              <img src="<?php echo base_url(); ?>assets/admin/images/users/avatar-4.jpg"
-                                 class="me-3 rounded-circle avatar-xs" alt="user-pic">
-                              <div class="flex-grow-1">
-                                 <h6 class="mb-1">Salena Layfield</h6>
-                                 <div class="font-size-12 text-muted">
-                                    <p class="mb-1" key="t-occidental">As a skeptical Cambridge friend of mine occidental.</p>
-                                    <p class="mb-0"><i class="mdi mdi-clock-outline"></i> <span key="t-hours-ago">1 hours ago</span></p>
-                                 </div>
-                              </div>
-                           </div>
-                        </a>
+                         
                      </div>
                      <div class="p-2 border-top d-grid">
                         <a class="btn btn-sm btn-link font-size-14 text-center" href="javascript:void(0)">
@@ -195,10 +133,9 @@
                   </button>
                   <div class="dropdown-menu dropdown-menu-end">
                      <!-- item-->
-                     <a class="dropdown-item" href="#"><i class="bx bx-user font-size-16 align-middle me-1"></i> <span key="t-profile">Profile</span></a>
-                     <a class="dropdown-item" href="#"><i class="bx bx-wallet font-size-16 align-middle me-1"></i> <span key="t-my-wallet">My Wallet</span></a>
-                     <a class="dropdown-item d-block" href="#"><span class="badge bg-success float-end">11</span><i class="bx bx-wrench font-size-16 align-middle me-1"></i> <span key="t-settings">Settings</span></a>
-                     <a class="dropdown-item" href="#"><i class="bx bx-lock-open font-size-16 align-middle me-1"></i> <span key="t-lock-screen">Lock screen</span></a>
+                     <a class="dropdown-item" href="<?php echo base_url()?>admin/dashboard"><i class="bx bx-user font-size-16 align-middle me-1"></i> <span key="t-profile">Profile</span></a>
+                     <a class="dropdown-item" href="<?php echo base_url()?>admin/profile/edit"><i class="bx bx-user font-size-16 align-middle me-1"></i> <span key="t-profile">Edit Profile</span></a>
+                     <a class="dropdown-item d-block" href="<?php echo base_url()?>admin/profile/change_passowrd"><i class="bx bx-wrench font-size-16 align-middle me-1"></i> <span key="t-settings">Change Password</span></a>
                      <div class="dropdown-divider"></div>
                      <a class="dropdown-item text-danger" href="<?php echo base_url()?>admin/login/logout"><i class="bx bx-power-off font-size-16 align-middle me-1 text-danger"></i> <span key="t-logout">Logout</span></a>
                   </div>
@@ -215,11 +152,13 @@
                <ul class="metismenu list-unstyled" id="side-menu">
                   <li>
                      <a  href="<?php echo base_url()?>admin/dashboard"   class="waves-effect">
+                     <i class="bx bx-home-circle"></i>
                      <span key="t-dashboards"> Dashboard</span>
                      </a>
                   </li>
                   <li>
                      <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-support"></i>
                      <span key="t-ecommerce">Customer Support</span>
                      </a>
                      <ul class="sub-menu" aria-expanded="false">
@@ -232,6 +171,7 @@
                   </li>
                   <li>
                      <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bxs-cart"></i>
                      <span key="t-ecommerce">Sales</span>
                      </a>
                      <ul class="sub-menu" aria-expanded="false">
@@ -241,15 +181,17 @@
                   </li>
                   <li>
                      <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bxs-shopping-bag"></i>
                      <span key="t-ecommerce">Purchase</span>
                      </a>
                      <ul class="sub-menu" aria-expanded="false">
-                        <li><a hidden href="<?php echo base_url('admin/customer/addnew');?>" key="t-products">menu1</a></li>
+                        <li><a   href="#" key="t-products">Add New Purchase</a></li>
                      </ul>
                   </li>
                   <li>
                      <a href="javascript: void(0);" class="has-arrow waves-effect">
-                     <span key="t-ecommerce">Farmers</span>
+                        <i class="bx bx-group"></i>
+                        <span key="t-ecommerce">Farmers</span>
                      </a>
                      <ul class="sub-menu" aria-expanded="false">
                         <li><a href="<?php echo base_url('admin/customer/addnew');?>" key="t-products">Add New Farmers</a></li>
@@ -258,6 +200,7 @@
                   </li>
                   <li>
                      <a href="javascript: void(0);" class="has-arrow waves-effect">
+                     <i class="bx bxs-file-doc"></i>
                      <span key="t-ecommerce">Documents</span>
                      </a>
                      <ul class="sub-menu" aria-expanded="false">
@@ -276,6 +219,8 @@
                       ?>
                   <li>
                      <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-cog"></i>
+                        
                      <span key="t-ecommerce">Settings</span>
                      </a>
                      <ul class="sub-menu" aria-expanded="false">
@@ -294,6 +239,7 @@
                   </li>
                   <li>
                      <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-duplicate"></i>
                      <span key="t-ecommerce">Type</span>
                      </a>
                      <ul class="sub-menu" aria-expanded="false">
@@ -306,13 +252,14 @@
                   <?php
                      }
                      ?>
-                  <li hidden>
+                  <li  >
                      <a href="javascript: void(0);" class="has-arrow waves-effect">
-                     <span key="t-ecommerce">Users</span>
+                     <i class="bx bx-user"></i>
+                     <span key="t-ecommerce">Profile</span>
                      </a>
                      <ul class="sub-menu" aria-expanded="false"  >
-                        <li><a href="<?php echo base_url('admin/users/list');?>" key="t-products">User list</a></li>
-                        <li><a href="<?php echo base_url('admin/users/addnew');?>" key="t-products">Add New Users</a></li>
+                        <li><a href="<?php echo base_url('admin/profile/edit');?>" key="t-products">Edit Profile</a></li>
+                        <li><a href="<?php echo base_url('admin/profile/change_passowrd');?>" key="t-products">Change Password</a></li>
                      </ul>
                   </li>
                </ul>
